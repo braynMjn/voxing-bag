f901da23e101e9b1df6d67f86c1946dfd1d97b6a
braynMjn <brainmjn11@gmail.com>
2020-12-22 19:32:11 +0545

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var snd = new Audio("sounds/PUNCH.mp3");
+new Vue({
+    el: '#vue-app',
+    data:{
+        health:100,
+        ended:false
+    },
+    methods:{
+        punch:function(){
+            snd.play();
+            this.health -=10;
+            if(this.health <=0){
+                this.ended = true;
+            }
+        },
+        restart:function(){
+            this.health=100;
+            this.ended =false;
+        }
+    },
+    computed:{
+
+    }
+}); 
